e3c9d2658678c6d1bd22719f1db0b0c9488ef33e
Art4 <art4@wlabs.de>
2020-05-01 11:38:55 +0200

    Spielregeln ergänzt

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import Reveal from 'reveal.js';
 
 import 'reveal.js/css/reveal.css';
 // Themes: beige, black, blood, league, moon, night, serif, simple, sky, solarized, white
-import 'reveal.js/css/theme/white.css';
+import 'reveal.js/css/theme/night.css';
 // Theme used for syntax highlighting of code
 import 'reveal.js/lib/css/zenburn.css';
+import './style.css';
 
 // configuration see https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
     history: true,
-    transition: 'slide', // none/fade/slide/convex/concave/zoom
+    transition: 'zoom', // none/fade/slide/convex/concave/zoom
     hideAddressBar: true,
     dependencies: [
         // DONT LOAD PLUGINS HERE!
         // use require() for plugins after Reveal.initialize()
     ],
-    // parallaxBackgroundImage: (window.location.search.match( /print-pdf/gi )) ? null :'img/background.jpg',
-    // parallaxBackgroundSize: '3648px 2056px',
+    // parallaxBackgroundImage: (window.location.search.match( /print-pdf/gi )) ? null :'img/haus-unordnung_1920.jpg',
+    // parallaxBackgroundSize: '1920px 1280px',
     pdfSeparateFragments: false,
 });
 
